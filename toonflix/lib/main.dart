bdf5503fb3db5d5cae356060db3e40dfc2ea27ca
walkerJung <jjh_walker@kakao.com>
2023-06-29 16:18:51 +0900

    feat: UI Buttons Section

--- toonflix/lib/main.dart
+++ toonflix/lib/main.dart
@@ -13,6 +13,7 @@ class App extends StatelessWidget {
         body: Padding(
           padding: EdgeInsets.symmetric(horizontal: 40),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               SizedBox(
                 height: 80,
@@ -33,12 +34,53 @@ class App extends StatelessWidget {
                       Text(
                         'Welcome back',
                         style: TextStyle(
-                            color: Colors.white.withOpacity(0.8), fontSize: 18),
+                          color: Colors.white.withOpacity(0.8),
+                        ),
                       ),
                     ],
                   )
                 ],
               ),
+              SizedBox(
+                height: 120,
+              ),
+              Text(
+                'Total Balance',
+                style: TextStyle(
+                  color: Colors.white.withOpacity(0.8),
+                  fontSize: 22,
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              Text(
+                '\$5 194 482',
+                style: TextStyle(
+                    color: Colors.white.withOpacity(0.8),
+                    fontSize: 48,
+                    fontWeight: FontWeight.w600),
+              ),
+              SizedBox(
+                height: 30,
+              ),
+              Row(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF2B33A),
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: Padding(
+                        padding:
+                            EdgeInsets.symmetric(vertical: 20, horizontal: 50),
+                        child: Text(
+                          'Transfer',
+                          style: TextStyle(fontSize: 22),
+                        )),
+                  )
+                ],
+              )
             ],
           ),
         ),
